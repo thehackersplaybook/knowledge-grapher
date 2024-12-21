6d9e5bddd04c4e2aa0978940b41b0c4e05d7e38a
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-21 17:02:25 +0530

    tests: remove example test

--- src/tests/example.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("example test", () => {
-  it("should add two numbers correctly", () => {
-    const sum = 1 + 1;
-    expect(sum).toBe(2);
-  });
-});
